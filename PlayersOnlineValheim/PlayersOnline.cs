1c5c4e6963c04433eba5d6215535a140890b7684
Emil Rydza <ekr@vicious.pro>
2021-03-07 10:01:43 +0100

    Labels now have colors and controlled widths

--- PlayersOnlineValheim/PlayersOnline.cs
+++ PlayersOnlineValheim/PlayersOnline.cs
@@ -14,21 +14,34 @@ namespace PlayersOnlineValheim
     {
         private static PlayersOnlinePlugin context;
         public static ConfigEntry<bool> modEnabled;
-        public static ConfigEntry<Color> fontColor;
+        public static ConfigEntry<string> playerListLocationString;
+        public static ConfigEntry<float> playerListNameWidth;
+        public static ConfigEntry<float> playerListDistanceWidth;
+
+        public static ConfigEntry<Color> nameFontColor;
+        public static ConfigEntry<Color> distanceFontColor;
         public static ConfigEntry<int> fontSize;
         public static ConfigEntry<string> fontName;
 
         public static Text text;
         private static Font font;
         private static GUIStyle style;
+        private static Vector2 playerListPosition;
 
         private void Awake()
         {
             context = this;
             modEnabled = Config.Bind<bool>("General", "Enabled", true, "Enable this mod");
+            playerListLocationString = Config.Bind<string>("General", "PlayerListLocationString", "85%,30%", "Location on the screen to show the list (x,y) or (x%,y%)");
+            playerListNameWidth = Config.Bind<float>("General", "PlayerListNameWidth", 150, "Width of the player name part of list");
+            playerListDistanceWidth = Config.Bind<float>("General", "PlayerListDistanceWidth", 120, "Width of the player distance part of list");
+
             fontName = Config.Bind<string>("General", "FontName", "AveriaSerifLibre-Bold", "Name of font");
             fontSize = Config.Bind<int>("General", "FontSize", 24, "Size of font");
-            fontColor = Config.Bind<Color>("General", "FontColor", Color.white, "Font color");
+            nameFontColor = Config.Bind<Color>("General", "NamefontColor", Color.white, "Name font color");
+            distanceFontColor = Config.Bind<Color>("General", "DistancefontColor", Color.gray, "Distance font color");
+
+            Config.Save();
 
             if (!modEnabled.Value)
                 return;
@@ -41,9 +54,23 @@ namespace PlayersOnlineValheim
             if (modEnabled.Value && Player.m_localPlayer && EnvMan.instance)
             {
                 float alpha = 1f;
-                string playerListString = GetPlayerListString();
-                style.normal.textColor = new Color(fontColor.Value.r, fontColor.Value.g, fontColor.Value.b, fontColor.Value.a * alpha);
-                GUI.Label(new Rect(new Vector2(Screen.width-200, (Screen.height/2 - 100)), new Vector2(200, 200)), playerListString, style);
+                //string playerListString = GetPlayerListString();
+
+                Vector2 nameLabelPosition = new Vector2(playerListPosition.x, playerListPosition.y);
+                style.normal.textColor = new Color(nameFontColor.Value.r, nameFontColor.Value.g, nameFontColor.Value.b, nameFontColor.Value.a * alpha);
+                GUI.Label(new Rect(nameLabelPosition, new Vector2(playerListNameWidth.Value, fontSize.Value)), "NAMETHATIS15CHA", style);
+
+                Vector2 distanceLabelPosition = new Vector2(playerListPosition.x + playerListNameWidth.Value, playerListPosition.y);
+                style.normal.textColor = new Color(distanceFontColor.Value.r, distanceFontColor.Value.g, distanceFontColor.Value.b, distanceFontColor.Value.a * alpha);
+                GUI.Label(new Rect(distanceLabelPosition, new Vector2(playerListDistanceWidth.Value, fontSize.Value)), "12345.6m", style);
+
+                Vector2 nameLabelPosition2 = new Vector2(playerListPosition.x, playerListPosition.y + fontSize.Value);
+                style.normal.textColor = new Color(nameFontColor.Value.r, nameFontColor.Value.g, nameFontColor.Value.b, nameFontColor.Value.a * alpha);
+                GUI.Label(new Rect(nameLabelPosition2, new Vector2(playerListNameWidth.Value, fontSize.Value)), "SECONDNAME", style);
+
+                Vector2 distanceLabelPosition2 = new Vector2(playerListPosition.x + playerListNameWidth.Value, playerListPosition.y + fontSize.Value);
+                style.normal.textColor = new Color(distanceFontColor.Value.r, distanceFontColor.Value.g, distanceFontColor.Value.b, distanceFontColor.Value.a * alpha);
+                GUI.Label(new Rect(distanceLabelPosition2, new Vector2(playerListDistanceWidth.Value, fontSize.Value)), "123.4m", style);
             }
         }
 
@@ -57,6 +84,9 @@ namespace PlayersOnlineValheim
 
         private static void ApplyConfig()
         {
+            string[] split = playerListLocationString.Value.Split(',');
+            playerListPosition = new Vector2(split[0].Trim().EndsWith("%") ? (float.Parse(split[0].Trim().Substring(0, split[0].Trim().Length - 1)) / 100f) * Screen.width : float.Parse(split[0].Trim()), split[1].Trim().EndsWith("%") ? (float.Parse(split[1].Trim().Substring(0, split[1].Trim().Length - 1)) / 100f) * Screen.height : float.Parse(split[1].Trim()));
+
             Font[] fonts = Resources.FindObjectsOfTypeAll<Font>();
             foreach (Font font in fonts)
             {
@@ -70,7 +100,7 @@ namespace PlayersOnlineValheim
             {
                 richText = true,
                 fontSize = fontSize.Value,
-                alignment = TextAnchor.MiddleLeft,
+                alignment = TextAnchor.UpperRight,
                 font = font
             };
         }
